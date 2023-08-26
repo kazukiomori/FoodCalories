96639fbe6561d91eaea04a8153a11cc93c312a0a
kazukiomori <kazuki1110170501@gmail.com>
2023-08-26 14:09:15 +0900

    Initial Commit

---
+++ FoodCalories/FoodCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodCaloriesApp.swift
+//  FoodCalories
+//
+//  Created by Kazuki Omori on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
