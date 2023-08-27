b23ae52afa3b612bd99f910a020ff0797f82c446
kazukiomori <kazuki1110170501@gmail.com>
2023-08-27 12:22:41 +0900

    [Add]ContentViewのUIとロジックを追加

--- FoodCalories/Views/ContentView.swift
+++ FoodCalories/Views/ContentView.swift
@@ -6,16 +6,76 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment (\.managedObjectContext) var managedObjContext
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+    
+    @State private var showingAddView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("\(Int(totalCalorieToday())) Kcal(今日)")
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                List {
+                    ForEach(food) { food in
+                        NavigationLink(destination: Text("\(food.calories)")) {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 6) {
+                                    Text("\(food.name!)")
+                                        .bold()
+                                    Text("\(Int(food.calories))") + Text("kcal")
+                                        .foregroundColor(.red)
+                                }
+                                Spacer()
+                                Text(calcTimeSince(date: food.date!))
+                                    .foregroundColor(.gray)
+                                    .italic()
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteFood)
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("FoodCalories")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddView.toggle()
+                    } label: {
+                        Label("追加", systemImage: "plus.circle")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView) {
+                AddFoodView()
+            }
         }
-        .padding()
+        .navigationViewStyle(.stack)
+    }
+    
+    private func deleteFood(offset: IndexSet) {
+        withAnimation {
+            offset.map { food[$0] }.forEach(managedObjContext.delete)
+            DataController().save(context: managedObjContext)
+        }
+    }
+    
+    private func totalCalorieToday() -> Double {
+        var todayCalories: Double = 0
+        for item in food {
+            if Calendar.current.isDateInToday(item.date!) {
+                todayCalories += item.calories
+            }
+        }
+        return todayCalories
     }
 }
 
